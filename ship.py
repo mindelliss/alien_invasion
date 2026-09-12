eb10c74bd46fd9914e04e85255deb176bbe229a0
Amir Mindelis <amir.mindeliss@gmail.com>
2026-09-13 00:23:25 +1000

    -bug fix: moving after reaching the right border -bug fix: stop moving after hitting left border

--- ship.py
+++ ship.py
@@ -24,7 +24,7 @@ class Ship:
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left and self.rect.right < self.screen_rect.right:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         
         self.rect.x = self.x
