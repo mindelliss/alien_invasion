ab643c69ae3481229029c4daeca231677f969610
Amir Mindelis <amir.mindeliss@gmail.com>
2026-09-13 23:53:19 +1000

    -limited number of bullets on the screen -delete bullets after leaving a screen -limit bullets to 3

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,7 +25,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
@@ -52,8 +52,15 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _check_keyup_events(self,event):
         if event.key == pygame.K_RIGHT:
